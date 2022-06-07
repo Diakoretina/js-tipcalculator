9d6d795b3da86ac7a61e85dc520b965ec8cc0c5d
Diakoretina <francesco.tammaro2000@gmail.com>
2022-06-07 16:50:48 +0200

    Fixed html and js

--- main.js
+++ main.js
@@ -1,32 +1,31 @@
 let inputBill = document.querySelector(".input__bill");
 let inputPeople = document.querySelector(".input__people");
-let customInput = document.queryCommandIndeterm(".custom__input");
+let customInput = document.querySelector(".custom__input");
 let tipMountOut = document.querySelector(".tip-mount-out");
 let totalOut = document.querySelector(".total-out");
 let buttonReset = document.getElementById(".reset-button");
-let boxTip = document.querySelectorAll(".preset")
-
+let boxTip = document.querySelectorAll(".preset");
 
 
 boxTip.forEach((element)=>{
     element.addEventListener("click", ()=>{
-        calculatorBill(inputBill.value, inputPeople.value, totalOut)
-        calculatorTip(inputBill.value, element, inputPeople.value, tipMountOut)
+      calculatorBill(inputBill.value, inputPeople.value, totalOut);
+      calculatorTip(inputBill.value, element, inputPeople.value, tipMountOut);
     })
-})
+  })
 
 function calculatorBill(bill, people, total){
     let resultBill = parseFloat(bill) / parseFloat(people);
-    total.innerHMTL = resultBill.toFixed(2);
+    total.innerHTML = resultBill.toFixed(2);
 }
 
-let tips = [5,10,15,25,50];
+let tips = [5, 10, 15, 25, 50]
 
 function calculatorTip(bill, boxTip, people, tipOut){
-    for(i=0; i<tips.length; i++){
-        if(boxTip.classList.contains(`${tips[i]}`)){
-            let resultMount = (parseFloat(bill) / parseFloat(people)) * `${tips[i]}` /100;
-            tipOut.innerHMTL = resultMount;
+for(i=0; i<tips.length; i++){
+    if(boxTip.classList.contains(`${tips[i]}`)){
+        let resultMount = (parseFloat(bill) / parseFloat(people)) * `${tips[i]}` / 100;
+        tipOut.innerHTML = resultMount
         }
     }
 }
